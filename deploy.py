321aaa722bf212faea96fdc4dea41dcf16b2a4fa
Erik Gafni <egafni@gmail.com>
2015-04-15 13:54:32 -0700

    start deploy

--- deploy.py
+++ deploy.py
@@ -1,24 +1,17 @@
-# import subprocess as sp
-# import os
-#
-# def main(new_version):
-#     def run(cmd):
-#         os.system(cmd)
-#
-#
-#     run('git flow release start %s' % new_version)
-#     run('git flow release finish %s' % new_version)
-#
-#     with open('cosmos/VERSION','w') as fh:
-#         fh.write(new_version)
-#
-#     run('python setup.py sdist upload')
-#     run('')
-#
-# if __name__ == '__main__':
-#     import argparse
-#     p = argparse.ArgumentParser()
-#     p.add_argument('new_version')
-#     args = p.parse_args()
-#
-#     main(**vars(args))
+import subprocess as sp
+import os
+
+def main(new_version):
+    def run(cmd):
+        os.system(cmd)
+
+    run('python setup.py sdist upload')
+    run('curl http://readthedocs.org/build/cosmos-wfm')
+
+if __name__ == '__main__':
+    import argparse
+    p = argparse.ArgumentParser()
+    #p.add_argument('new_version')
+    args = p.parse_args()
+
+    main(**vars(args))
