d88db939cf243a0717eb163b566dec150fafea45
Erik Gafni <egafni@gmail.com>
2015-08-30 15:01:41 -0700

    move stuff around

--- cosmos/api.py
+++ cosmos/api.py
@@ -6,6 +6,9 @@ from .models.Stage import Stage
 from .models.Tool import Tool, Input, Inputs
 from .models.Execution import Execution
 from .util.args import add_execution_args
-from .util.tool import one2one, one2many, many2one, many2many, make_dict
+from .util.relationship_patterns import one2one, one2many, many2one
+from util.helpers import make_dict
+
+
 from .graph.draw import draw_task_graph, draw_stage_graph, pygraphviz_available
 from .util.iterstuff import only_one
