58c01e93f51834f45f1ae07c61b619f5aefc32ed
Erik Gafni <egafni@gmail.com>
2015-08-31 11:42:24 -0700

    api imports

--- cosmos/api.py
+++ cosmos/api.py
@@ -1,14 +1,16 @@
 from . import *
 
-from .core.cmd_fxn.io import find, out_dir, abstract_input_taskfile, abstract_output_taskfile, forward, abstract_output_taskfile_old
+from .core.cmd_fxn.io import find, out_dir, forward
+from . import Cosmos
+
 from .models.Task import Task
 from .models.Stage import Stage
-from .models.Tool import Tool, Input, Inputs
 from .models.Execution import Execution
-from .util.args import add_execution_args
-from .util.relationship_patterns import one2one, one2many, many2one
-from util.helpers import make_dict
 
+
+from .util.args import add_execution_args
+from .util.relationship_patterns import one2one, one2many, many2one, group
+from .util.helpers import make_dict
+from .util.iterstuff import only_one
 
 from .graph.draw import draw_task_graph, draw_stage_graph, pygraphviz_available
-from .util.iterstuff import only_one
