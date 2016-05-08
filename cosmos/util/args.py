ba346b7d032ba97e42049dddb876cf93f26f52f5
Matthew Pearson <matthewpearson@gmail.com>
2016-05-08 11:23:22 -0700

    Add an option to add_workflow_args() that makes --name optional.

--- cosmos/util/args.py
+++ cosmos/util/args.py
@@ -7,11 +7,8 @@ def get_last_cmd_executed():
     return ' '.join([os.path.basename(sys.argv[0])] + cmd_args)
 
 #
-def add_workflow_args(parser):
-    p = parser
-    # parser.add_argument('-o', '--output_dir', type=str, help="The directory to output files to.  Path should not exist if this is a new workflow.")
-
-    p.add_argument('--name', '-n', help="A name for this workflow", required=True)
+def add_workflow_args(p, require_name=True):
+    p.add_argument('--name', '-n', help="A name for this workflow", required=require_name)
     p.add_argument('--max_cores', '-c', type=int,
                    help="Maximum number (based on the sum of cpu_requirement) of cores to use at once.  0 means unlimited", default=None)
     p.add_argument('--max_attempts', '-a', type=int,
@@ -20,5 +17,3 @@ def add_workflow_args(parser):
                    help="Completely restart the workflow.  Note this will delete all record of the workflow in the database")
     p.add_argument('--skip_confirm', '-y', action='store_true',
                    help="Do not use confirmation prompts before restarting or deleting, and assume answer is always yes")
-
-
