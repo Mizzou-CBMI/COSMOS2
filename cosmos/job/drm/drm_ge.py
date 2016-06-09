d5366a6f20fc4a7f7bec5db5c23b7a7c966b90c1
Erik Gafni <egafni@gmail.com>
2016-06-08 17:59:24 -0700

    bump

--- cosmos/job/drm/drm_ge.py
+++ cosmos/job/drm/drm_ge.py
@@ -134,7 +134,7 @@ def qacct(task, timeout=600):
                 if len(qacct_out.strip()):
                     break
                 else:
-                    task.workflow.log.warn('qacct -j %s returned an empty string for %s'  % (task.drm_jobID, task))
+                    task.workflow.log.warn('`qacct -j %s` returned an empty string for %s' % (task.drm_jobID, task))
             except sp.CalledProcessError:
                 pass
             time.sleep(5)
