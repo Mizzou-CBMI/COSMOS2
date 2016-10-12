39a91716c93aacd9f24e051fa8005749059cdb0d
Erik Gafni <egafni@gmail.com>
2016-10-11 17:19:58 -0700

    ecs: ecs

--- cosmos/job/drm/drm_ecs.py
+++ cosmos/job/drm/drm_ecs.py
@@ -20,11 +20,9 @@ class DRM_ECS(DRM):
         import boto3
         self.ecs = boto3.client('ecs')
         super(DRM_ECS, self).__init__(*args, **kwargs)
-        self.drm_options = dict(cluster='pipe-dev',
-                                container_image='pipe-dev:PIPE-2139-docker_v18',
-                                task_family='pipe-job-dev',
-                                mount_points=[{u'containerPath': u'/locus', u'sourceVolume': u'efs'}],
-                                startedBy='cosmos')
+
+        for k in ('cluster', 'container_image', 'task_family', 'mounts', 'started_by'):
+            assert k in self.drm_options, '%s must be set in the ECS drm_options' % k
 
         self.task_id_to_task_definition_arn = dict()
 
@@ -42,28 +40,32 @@ class DRM_ECS(DRM):
                                           u'essential': True,
                                           u'image': self.drm_options['container_image'],
                                           u'memoryReservation': task.mem_req or 1000,
-                                          u'mountPoints': self.drm_options['mount_points'],
+                                          u'mountPoints': [m[''] for m in self.drm_options['mounts']],
                                           u'name': task.stage.name,
                                           u'portMappings': [],
                                           u'readonlyRootFilesystem': False,
                                           u'volumesFrom': []}],
                u'family': self.drm_options['task_family'],
                u'networkMode': u'bridge',
-               u'volumes': [{u'host': {u'sourcePath': u'/locus'}, u'name': u'efs'}]}
+               u'volumes': [{u'host': {u'sourcePath': m['sourcePath']}, u'name': m['name']} for m in self.drm_options['mounts']]}
+            # u'volumes': [{u'host': {u'sourcePath': u'/locus'}, u'name': u'efs'}]}
         )
 
-        _check_status(r)
+        _check_response_for_error(r)
         self.task_id_to_task_definition_arn[task.id] = r['taskDefinition']['taskDefinitionArn']
 
         r = self.ecs.run_task(cluster=self.drm_options['cluster'],
                               taskDefinition=r['taskDefinition']['taskDefinitionArn'],
-                              startedBy=self.drm_options['startedBy'])
+                              startedBy=self.drm_options['started_by'])
+        _check_response_for_error(r)
+
         drm_jobID = only_one(r['tasks'])['taskArn']
         # ns = ' ' + task.drm_native_specification if task.drm_native_specification else ''
         return drm_jobID
 
     def clean_up(self, task):
-        self.ecs.deregister_task_definition(taskDefinition=self.task_id_to_task_definition_arn.pop(task.id))
+        if task.id in self.task_id_to_task_definition_arn:
+            self.ecs.deregister_task_definition(taskDefinition=self.task_id_to_task_definition_arn.pop(task.id))
 
     def filter_is_done(self, tasks):
         """
@@ -85,19 +87,20 @@ class DRM_ECS(DRM):
 
     def kill_tasks(self, tasks):
         for task in tasks:
-            r = self.ecs.stop_task(
-                cluster=self.drm_options['cluster'],
-                task=task.drm_jobID,
-                reason='killed by cosmos'
-            )
-
-            _check_status(r)
+            if task.drm_jobID is not None:
+                r = self.ecs.stop_task(
+                    cluster=self.drm_options['cluster'],
+                    task=task.drm_jobID,
+                    reason='killed by cosmos'
+                )
+            _check_response_for_error(r)
+            self.clean_up(task)
 
     def _describe_tasks(self, tasks):
         r = self.ecs.describe_tasks(cluster=self.drm_options['cluster'], tasks=[task.drm_jobID for task in tasks])
 
         # Error checking
-        _check_status(r)
+        _check_response_for_error(r)
 
         return {t: task_r for t, task_r in zip(tasks, r['tasks'])}
 
@@ -109,7 +112,7 @@ class DRM_ECS(DRM):
         return {t: task_r['lastStatus'] for t, task_r in self._describe_tasks(tasks)}
 
 
-def _check_status(r):
+def _check_response_for_error(r):
     if 'failures' in r and len(r['failures']):
         raise Exception('Failures:\n{0}'.format(pprint.pformat(r['failures'], indent=2)))
 
