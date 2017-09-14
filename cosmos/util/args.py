131969e364e75b33b52e99a1940191dcb623311c
Matthew Pearson <matthewpearson@gmail.com>
2017-09-14 15:25:16 -0700

    Remove max_attempts from add_workflow_args().

--- cosmos/util/args.py
+++ cosmos/util/args.py
@@ -1,4 +1,3 @@
-import os
 import sys
 
 
@@ -6,13 +5,11 @@ def get_last_cmd_executed():
     cmd_args = [a if ' ' not in a else "'" + a + "'" for a in sys.argv[1:]]
     return ' '.join([sys.argv[0]] + cmd_args)
 
-#
+
 def add_workflow_args(p, require_name=True):
     p.add_argument('--name', '-n', help="A name for this workflow", required=require_name)
     p.add_argument('--max_cores', '--max-cores', '-c', type=int,
                    help="Maximum number (based on the sum of Task.core_req) of cores to use at once.  0 means unlimited", default=None)
-    p.add_argument('--max_attempts', '--max-attempts', '-a', type=int,
-                   help="Default number of times to try running a Task that must succeed before the workflow fails", default=1)
     p.add_argument('--restart', '-r', action='store_true',
                    help="Completely restart the workflow.  Note this will delete all record of the workflow in the database")
     p.add_argument('--skip_confirm', '--skip-confirm', '-y', action='store_true',
