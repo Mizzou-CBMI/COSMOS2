d18693d94ea88d1ac46dd527341cea9d27c04f70
Erik Gafni <egafni@gmail.com>
2020-12-18 09:02:59 -0800

    use main

--- misc/massive_workflow.py
+++ misc/massive_workflow.py
@@ -22,33 +22,38 @@ def silly_recipe(wf, i, num_things):
         )
 
 
-# start cosmos engine
-cosmos = Cosmos(
-    database_url="sqlite://",
-    default_drm="local",
-    # default_drm="ge",
-    default_queue="dev-short",
-    default_drm_options={},
-    get_submit_args=partial(default_get_submit_args, parallel_env="smp"),
-)
-cosmos.initdb()
+def main():
+    # start cosmos engine
+    cosmos = Cosmos(
+        database_url="sqlite://",
+        default_drm="local",
+        # default_drm="ge",
+        default_queue="dev-short",
+        default_drm_options={},
+        get_submit_args=partial(default_get_submit_args, parallel_env="smp"),
+    )
+    cosmos.initdb()
 
-# create cosmos workflow
-workflow = cosmos.start(
-    # NOTE cosmos will make dirs in this path
-    # primary_log_path=os.path.join("logs", "cosmos.log"),
-    name="blah",
-    restart=True,
-    skip_confirm=True,
-    fail_fast=True,
-)
+    # create cosmos workflow
+    workflow = cosmos.start(
+        # NOTE cosmos will make dirs in this path
+        # primary_log_path=os.path.join("logs", "cosmos.log"),
+        name="blah",
+        restart=True,
+        skip_confirm=True,
+        fail_fast=True,
+    )
 
-for i in range(100):
-    print("add {}".format(i))
-    silly_recipe(workflow, i, 100)
+    for i in range(100):
+        print("add {}".format(i))
+        silly_recipe(workflow, i, 100)
 
-workflow.make_output_dirs()
+    workflow.make_output_dirs()
 
-# run cosmos workflow
-# with SGESignalHandler(workflow):
-workflow.run()
+    # run cosmos workflow
+    # with SGESignalHandler(workflow):
+    workflow.run()
+
+
+if __name__ == "__main__":
+    main()
