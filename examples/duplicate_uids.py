cf8397d99084c0258fb931bdadb82b1750116e8a
Erik Gafni <egafni@gmail.com>
2020-12-18 10:51:34 -0800

    fixup example

--- examples/duplicate_uids.py
+++ examples/duplicate_uids.py
@@ -6,40 +6,41 @@ from cosmos.models.Workflow import DuplicateUid, InvalidParams
 environment_variables_dict = {"Cosmos": "jobs", "are": "very", "cool": "!"}
 
 
-def cmd(a):
+def prepare_data(a):
     print(a)
 
 
-def cmd2(a):
+def train_machine_learning_model(a):
     print(a)
 
 
 def main():
     cosmos = Cosmos().initdb()
     workflow = cosmos.start("duplicate_uids", skip_confirm=True)
-    task = workflow.add_task(func=cmd, params=dict(a=1), uid="x")
+    task = workflow.add_task(func=prepare_data, params=dict(a=1), uid="x")
 
     # normally you can't add a task with the same uid to the same stage
     with pytest.raises(DuplicateUid):
-        workflow.add_task(func=cmd, params=dict(a=1), uid="x")
+        workflow.add_task(func=prepare_data, params=dict(a=1), uid="x")
 
     # normally you can't add a task with the same uid to the same stage
     with pytest.raises(DuplicateUid):
-        workflow.add_task(func=cmd, params=dict(a=1), uid="x")
+        workflow.add_task(func=prepare_data, params=dict(a=1), uid="x")
 
     # set if_duplicate="return" to True to get the same task back that you added
-    task2 = workflow.add_task(func=cmd, params=dict(a=1), uid="x", if_duplicate="return")
+    task2 = workflow.add_task(func=prepare_data, params=dict(a=1), uid="x", if_duplicate="return")
     assert task == task2
 
-    # this can be especially useful in loops
+    # this can be especially useful in loops to avoid repeating computation
     for _ in range(3):
-        # set if_duplicate="return" to True to get the same task back that you added
-        task = workflow.add_task(func=cmd, params=dict(a=1), uid="x", if_duplicate="return")
-        workflow.add_task(func=cmd2, params=dict(a=1), uid="x", if_duplicate="return", parents=task)
+        task = workflow.add_task(func=prepare_data, params=dict(a=1), uid="x", if_duplicate="return")
+        workflow.add_task(
+            func=train_machine_learning_model, params=dict(a=1), uid="x", if_duplicate="return", parents=task
+        )
 
-    # parameters must be identical
+    # NOTE: parameters must be identical when using this feature
     with pytest.raises(InvalidParams):
-        workflow.add_task(func=cmd2, params=dict(a=1000), uid="x", if_duplicate="return")
+        workflow.add_task(func=prepare_data, params=dict(a=1000), uid="x", if_duplicate="return")
 
 
 if __name__ == "__main__":
