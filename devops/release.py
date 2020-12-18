3e18813cbd7419740318aa76c718cdd848b9fec4
Erik Gafni <egafni@gmail.com>
2020-12-18 12:57:36 -0800

    update release.py

--- devops/release.py
+++ devops/release.py
@@ -18,45 +18,47 @@ def main(args):
 
     conda create -n cosmos
     conda activate cosmos
-    conda install conda-build python>3
+    conda install conda-build ghp-import sphinx sphinx_rtd_theme python>3
     python setup.py develop
 
-    AFTER cosmos/VERSION is bumped
-    devops/release.py
+    AFTER cosmos/VERSION is bumped and committed you can run:
+    $ devops/release.py
     """
 
     if not args.skip_pypi:
         run("python setup.py sdist upload")
 
-    # run("curl http://readthedocs.org/build/cosmos-wfm")
+    if not args.skip_conda:
+        if os.path.exists("cosmos-wfm"):
+            print("removing cosmos-wfm dir")
+            rmtree("cosmos-wfm")
 
-    if os.path.exists("cosmos-wfm"):
-        print("removing cosmos-wfm dir")
-        rmtree("cosmos-wfm")
+        run(f"conda skeleton pypi cosmos-wfm --version {__version__}")
+        run(f"conda build cosmos-wfm")
 
-    run(f"conda skeleton pypi cosmos-wfm --version {__version__}")
-    run(f"conda build cosmos-wfm")
+        # conda base dir, ex: /home/egafni/miniconda3
+        conda_base = subprocess.check_output("conda info --base", shell=True)
 
-    # conda base dir, ex: /home/egafni/miniconda3
-    conda_base = subprocess.check_output("conda info --base", shell=True)
+        run(
+            f"anaconda upload {conda_base}/conda-bld/linux-64/cosmos-wfm-{__version__}-py38_0.tar.bz2 -u ravelbio"
+        )
 
-    run(
-        f"anaconda upload {conda_base}/conda-bld/linux-64/cosmos-wfm-{__version__}-py38_0.tar.bz2 -u ravelbio"
-    )
-
-    # reqiures pip install ghp-import sphinx sphinx_rtd_theme
-    run(
-        f"""
-            cd docs
-            make html
-            cd build/html
-            ghp-import -n ./ -p        
-        """
-    )
+    if not args.skip_docs:
+        # requires packages: ghp-import sphinx sphinx_rtd_theme
+        run(
+            f"""
+                cd docs
+                make html
+                cd build/html
+                ghp-import -n ./ -p        
+            """
+        )
 
 
 if __name__ == "__main__":
     p = ArgumentParser()
     p.add_argument("--skip-pypi", action="store_true")
+    p.add_argument("--skip-conda", action="store_true")
+    p.add_argument("--skip-docs", action="store_true")
     args = p.parse_args()
     main(args)
